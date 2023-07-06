e1681bc7eee53743123d59a09a01cbdde7b3b42c
Damien LeBerrigaud <github@itsdamo.com>
2023-07-06 07:51:38 -0600

    More styling

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.indigo.shade900,
+          brightness: MediaQuery.of(context).platformBrightness,
+        ),
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(fontWeight: FontWeight.w500, letterSpacing: 0.5),
+        ),
         useMaterial3: true,
       ),
       home: const LocationsPage(),
@@ -55,21 +61,40 @@ class _LocationsPageState extends State<LocationsPage> {
     });
   }
 
-  Widget _singleSearchResultWidget(Location location) {
-    return Row(children: [
-      Text('${location.name}, ${location.region}'),
-    ]);
+  Widget _singleSearchResultWidget(BuildContext context, Location location) {
+    var theme = Theme.of(context);
+    var borderColor = theme.colorScheme.outline;
+
+    return ListTile(
+      title: Text(location.name),
+      titleTextStyle: theme.textTheme.titleMedium,
+      subtitle: Text(location.region),
+      subtitleTextStyle: theme.textTheme.labelMedium,
+      shape: Border(bottom: BorderSide(color: borderColor)),
+    );
   }
 
-  Widget _searchResultsWidget(List<Location> locations) {
-    return Column(
-      children: locations.map((location) => _singleSearchResultWidget(location)).toList(),
+  Widget _searchResultsWidget(BuildContext context, List<Location> locations) {
+    return Expanded(
+      child: ListView(
+        children: locations.map((location) => _singleSearchResultWidget(context, location)).toList(),
+      ),
     );
   }
 
   Widget _searchErrorWidget(HttpError error) => Text(error.message());
 
-  Widget _loadingWidget() => const SizedBox(width: 32, height: 32, child: CircularProgressIndicator());
+  Widget _loadingWidget() => Expanded(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 128),
+              child: const SizedBox(width: 32, height: 32, child: CircularProgressIndicator()),
+            ),
+          ],
+        ),
+      );
 
   Widget _searchResultsFutureWidget() {
     if (_searchFuture == null) {
@@ -79,7 +104,7 @@ class _LocationsPageState extends State<LocationsPage> {
     return FutureBuilder(
       future: _searchFuture!,
       builder: (context, snapshot) => switch (snapshot.data) {
-        Ok(value: var locations) => _searchResultsWidget(locations),
+        Ok(value: var locations) => _searchResultsWidget(context, locations),
         Err(error: var err) => _searchErrorWidget(err),
         null => _loadingWidget(),
       },
@@ -88,22 +113,28 @@ class _LocationsPageState extends State<LocationsPage> {
 
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var colorScheme = theme.colorScheme;
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: colorScheme.inversePrimary,
         title: const Text('Weather Locations'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            color: colorScheme.secondaryContainer,
+            padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+            child: TextField(
               controller: _searchTextEditController,
               onSubmitted: (String value) => _startSearch(value),
+              textInputAction: TextInputAction.search,
             ),
-            _searchResultsFutureWidget(),
-          ],
-        ),
+          ),
+          _searchResultsFutureWidget(),
+        ],
       ),
     );
   }
